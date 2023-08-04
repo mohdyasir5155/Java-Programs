e1ad0e28e209cacf29fbf302c6d6f7ded4dd8c11
Mohd Yasir Sheikh <mohdyasir5155@gmail.com>
2023-08-04 20:05:09 +0530

    Add files via upload

---
+++ Java_Else-if/yasir.java
@@ -0,0 +1,12 @@
+public class yasir {
+    public static void main(String[] args) {
+        int time = 22;
+        if(time < 10){
+            System.out.println("Good Morning");
+        }else if(time < 18){
+            System.out.println("Good Day.");
+        }else {
+            System.out.println("Good Evening!");
+        }
+    }
+}
