1e716ee7d815809c00cada17ff75cb83138bd4ce
Mohd Yasir Sheikh <mohdyasir5155@gmail.com>
2023-08-04 19:27:58 +0530

    Add files via upload

---
+++ Java_Operator/main.java
@@ -0,0 +1,11 @@
+public class main {
+  public static void main(String[]args){
+        int sum1 = 100 + 50;
+        int sum2 = sum1 + 250;
+        int sum3 = sum2 + sum1;
+
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+    }
+}
