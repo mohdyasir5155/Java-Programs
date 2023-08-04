f208062b4afc337ff077b72023d39b0d1c4407f4
Mohd Yasir Sheikh <mohdyasir5155@gmail.com>
2023-08-04 18:56:02 +0530

    Add files via upload

---
+++ Java_Narrowing_Casting/code.java
@@ -0,0 +1,14 @@
+public class code{
+    public static void main(String[] args) {
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble;
+
+        System.out.println(myDouble); //Output 9.78
+        System.out.println(myInt);    // Output 9
+    }
+}
+
+//Narrowing Casting
+//Narrowing casting must be done manually by placing the type in parentheses in front of the value:
+
+//By Mohd Yasir Sheikh
