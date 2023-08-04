db2ec3306f4e7863e42a48f86258eb523e5bedf6
Mohd Yasir Sheikh <mohdyasir5155@gmail.com>
2023-08-04 14:48:01 +0530

    Add files via upload

---
+++ Integer_Type_Number/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        byte myBye =  99;
+        System.out.println(myBye);
+
+        short myShort = 500;
+        System.out.println(myShort);
+
+        int myInt = 100000;
+        System.out.println(myInt);
+
+        long myLong =1500000000000L;
+        System.out.println(myLong);
+
+        float myFloat = 5.75f;
+        System.out.println(myFloat);
+
+        double myDouble = 19.99d;]
+        System.out.println(myDouble);
+    }
+}
+//By Mohd Yasir Sheikh
---
+++ String_2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Yasir";
+        String lastName = "Sheikh";
+        String fullName = name + lastName;
+        System.out.println(fullName);
+    }
+}
