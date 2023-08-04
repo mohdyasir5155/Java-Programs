9a44912ca0fa67c606f493b7266cef77b1b8a104
Mohd Yasir Sheikh <mohdyasir5155@gmail.com>
2023-08-04 19:59:15 +0530

    Update yasir.java

--- Boolean_Example/yasir.java
+++ Boolean_Example/yasir.java
@@ -3,5 +3,10 @@ public class yasir {
         int myAge = 25;
         int votingAge = 18; 
         System.out.println(myAge >= votingAge);
+        
+        System.out.println("Old enough to vote!");
+        } else {
+        System.out.println("Not old enough to vote.");
+        }
     }
 }
